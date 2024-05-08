44ec9e04f51b502b71004f4a700eceacdce22bec
Pablo Henrique <pablohdsouza.dev@gmail.com>
2024-05-07 22:18:13 -0300

    feat: add QueryClientProvider and ReactQueryDevtools in main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,20 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/theme/default.ts";
 import { GlobalStyle } from "./styles/global.ts";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./tanstackQuery/queryClient.ts";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-      <GlobalStyle />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={defaultTheme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+        <GlobalStyle />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
